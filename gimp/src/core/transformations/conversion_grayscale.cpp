7f766a03c28f8d849a5693328e4917d29b972f42
https://pestek@bitbucket.org <matekplo@wp.pl>
2015-10-20 20:39:26 +0200

    cwiczenie 2

--- gimp/src/core/transformations/conversion_grayscale.cpp
+++ gimp/src/core/transformations/conversion_grayscale.cpp
@@ -17,16 +17,59 @@ PNM* ConversionGrayscale::transform()
     int width = image->width();
     int height = image->height();
 
-    PNM* newImage = new PNM(width, height, QImage::Format_Indexed8);
+     PNM* newImage = new PNM(width, height, image->format());
 
-    if (image->format() == QImage::Format_Mono)
-    {
+     if (image->format() == QImage::Format_Mono)
+     {
+         // Iterate over image space
+         for (int x=0; x<width; x++)
+             for (int y=0; y<height; y++)
+             {
+                 QColor color = QColor::fromRgb(image->pixel(x,y)); // Getting the pixel(x,y) value
+
+                 newImage->setPixel(x,y, color == Qt::white ? Qt::color1 : Qt::color0);
+             }
+     }
+     else if (image->format() == QImage::Format_Indexed8)
+     {
+         // Iterate over image space
+         for (int x=0; x<width; x++)
+             for (int y=0; y<height; y++)
+             {
+                 QRgb pixel = image->pixel(x,y); // Getting the pixel(x,y) value
+
+                 int v = qGray(pixel);    // Get the 0-255 value of the L channel
+
+                 newImage->setPixel(x,y, v);
+             }
+     }
+     else //if (image->format() == QImage::Format_RGB32)
+     {
+
+        for (int x=0; x<width; x++)
+            for (int y=0; y<height; y++)
+            {
+                QRgb pixel = image->pixel(x,y); // Getting the pixel(x,y) value
+
+                int r = qRed(pixel);    // Get the 0-255 value of the R channel
+                int g = qGreen(pixel);  // Get the 0-255 value of the G channel
+                int b = qBlue(pixel);   // Get the 0-255 value of the B channel
+
+                double srednia = r*0.3 + g*0.6 + b*0.1;
+
+                r = srednia;
+                g = srednia;
+                b = srednia;
+                QColor newPixel = QColor(r,g,b);
+                newImage->setPixel(x,y, newPixel.rgb());
+            }
 
     }
-    else // if (image->format() == QImage::Format_RGB32)
-    {
 
-    }
+
+
+
+
 
     return newImage;
 }
--- gimp/src/core/transformations/correction.cpp
+++ gimp/src/core/transformations/correction.cpp
@@ -1,4 +1,5 @@
 #include "correction.h"
+#include <QtCore/qmath.h>
 
 Correction::Correction(PNM* img) :
     Transformation(img)
@@ -21,7 +22,68 @@ PNM* Correction::transform()
 
     PNM* newImage = new PNM(width, height, image->format());
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    // wypełnienie tablicy LUT
+    for (int x=0; x<PIXEL_VAL_MAX+1; x++)
+    {
+        LUT[x] = x;
+    }
+
+    //
+    for (int x=0; x<PIXEL_VAL_MAX+1; x++)
+    {
+        LUT[x] = LUT[x] + shift;
+        LUT[x] = LUT[x] * factor;
+        LUT[x] = qPow(LUT[x],gamma);
+        LUT[x] = (LUT[x] > PIXEL_VAL_MAX) ? PIXEL_VAL_MAX : LUT[x];
+        LUT[x] = (LUT[x] < PIXEL_VAL_MIN) ? PIXEL_VAL_MIN : LUT[x];
+    }
+
+
+    if (image->format() == QImage::Format_Mono)
+    {
+
+        for (int x=0; x<width; x++)
+            for (int y=0; y<height; y++)
+            {
+                QColor color = QColor::fromRgb(image->pixel(x,y));
+
+                newImage->setPixel(x,y, color == Qt::white ? LUT[PIXEL_VAL_MAX] : LUT[PIXEL_VAL_MIN]);
+            }
+    }
+    else if (image->format() == QImage::Format_Indexed8)
+    {
+
+        for (int x=0; x<width; x++)
+            for (int y=0; y<height; y++)
+            {
+                QRgb pixel = image->pixel(x,y);
+
+                int v = qGray(pixel);
+                v = LUT[v];
+
+                newImage->setPixel(x,y, v);
+            }
+    }
+    else //if (image->format() == QImage::Format_RGB32)
+    {
+
+        for (int x=0; x<width; x++)
+            for (int y=0; y<height; y++)
+            {
+                QRgb pixel = image->pixel(x,y);
+
+                int r = qRed(pixel);
+                int g = qGreen(pixel);
+                int b = qBlue(pixel);
+
+                r = LUT[r];
+                g = LUT[g];
+                b = LUT[b];
+
+                QColor newPixel = QColor(r,g,b);
+                newImage->setPixel(x,y, newPixel.rgb());
+            }
+    }
 
     return newImage;
 }
