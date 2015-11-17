bbc844d641a7727bb42ad99f4a4eddebd5a94dac
Oskar Ziegenhagen <oskar1501@gmail.com>
2015-11-17 13:16:19 +0100

    cwiczenia 6 - odszumianie - part 1

--- gimp/src/core/transformations/noise_bilateral.cpp
+++ gimp/src/core/transformations/noise_bilateral.cpp
@@ -21,28 +21,95 @@ PNM* NoiseBilateral::transform()
     sigma_r = getParameter("sigma_r").toInt();
     radius = sigma_d;
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+
+
+    if (image->format() == QImage::Format_Indexed8)
+    {
+
+        for (int x=0; x<width; x++)
+            for (int y=0; y<height; y++)
+            {
+                QRgb pixel = image->pixel(x,y); // Getting the pixel(x,y) value
+
+                int v = qGray(pixel);    // Get the 0-255 value of the L channel
+                v = calcVal(x,y,LChannel);
+
+                newImage->setPixel(x,y, v);
+            }
+
+    }
+    else if (image->format() == QImage::Format_RGB32)
+    {
+
+        for (int x=0; x<width; x++)
+            for (int y=0; y<height; y++)
+            {
+                QRgb pixel = image->pixel(x,y); // Getting the pixel(x,y) value
+
+                int r = qRed(pixel);    // Get the 0-255 value of the R channel
+                int g = qGreen(pixel);  // Get the 0-255 value of the G channel
+                int b = qBlue(pixel);   // Get the 0-255 value of the B channel
+
+                r = calcVal(x,y,RChannel);
+                g = calcVal(x,y,GChannel);
+                b = calcVal(x,y,BChannel);
+
+                QColor newPixel = QColor(r,g,b);
+                newImage->setPixel(x,y, newPixel.rgb());
+            }
+
+    }
+
 
     return newImage;
 }
 
 int NoiseBilateral::calcVal(int x, int y, Channel channel)
 {
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    math::matrix<float> okno = Transformation::getWindow(x,y,radius,channel,RepeatEdge);
 
-    return 0;
+
+    QPoint p,point;
+    p.setX(x);
+    p.setY(y);
+
+
+    float suma_licznik = 0;
+    float suma_mianownik = 0;
+    for (int i=0; i<radius; i++){
+        for (int j=0; j<radius; j++){
+            point.setX(x-radius/2 + i);
+            point.setY(y-radius/2 + j);
+            suma_licznik += okno(i,j) * colorCloseness(okno(i,j),okno(radius/2,radius/2)) * spatialCloseness(point, p);
+            suma_mianownik += colorCloseness(okno(i,j),okno(radius/2,radius/2)) * spatialCloseness(point, p);
+        }
+    }
+
+    int result = suma_licznik / suma_mianownik;
+
+    if (result > PIXEL_VAL_MAX)
+        result = PIXEL_VAL_MAX;
+    else if (result < PIXEL_VAL_MIN)
+        result = PIXEL_VAL_MIN;
+
+    return result;
 }
 
 float NoiseBilateral::colorCloseness(int val1, int val2)
 {
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    float param, result;
+    param = ( (float)( (val1 - val2) * (val1 - val2) )  /  (float)( 2 * sigma_r * sigma_r ) ) * (-1);
+    result = exp(param);
 
-    return 0;
+    return result;
 }
 
 float NoiseBilateral::spatialCloseness(QPoint point1, QPoint point2)
 {
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
 
-    return 0;
+    float param, result;
+    param = ( (float)(  (point1.x() - point2.x())  *   (point1.x() - point2.x()) +  (point1.y() - point2.y()) *  (point1.y() - point2.y())   )  /  (float)(2*sigma_d*sigma_d) ) * (-1);
+    result = exp(param);
+
+    return result;
 }
--- gimp/src/core/transformations/noise_median.cpp
+++ gimp/src/core/transformations/noise_median.cpp
@@ -17,7 +17,34 @@ PNM* NoiseMedian::transform()
 
     PNM* newImage = new PNM(width, height, image->format());
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    Channel RR = RChannel; //0
+    Channel GG = GChannel; //1
+    Channel BB = BChannel; //2
+
+    if (image->format() == QImage::Format_Indexed8)
+    {
+
+    }
+    else if (image->format() == QImage::Format_RGB32)
+    {
+
+        for (int x=0; x<width; x++)
+            for (int y=0; y<height; y++)
+            {
+                QRgb pixel = image->pixel(x,y); // Getting the pixel(x,y) value
+
+                // wywołujemy okna sąsiedztwa dla każdego kanału
+                int r = getMedian(x,y,RR);
+                int g = getMedian(x,y,GG);
+                int b = getMedian(x,y,BB);
+
+                QColor newPixel = QColor(r,g,b);
+                newImage->setPixel(x,y, newPixel.rgb());
+
+            }
+
+    }
+
 
     return newImage;
 }
@@ -25,8 +52,46 @@ PNM* NoiseMedian::transform()
 int NoiseMedian::getMedian(int x, int y, Channel channel)
 {
     int radius = getParameter("radius").toInt();
+    radius = radius * 2 + 1;
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    int zlicz = 0;
 
-    return 0;
+    float tab[radius*radius + 1];
+
+    math::matrix<float> okno = Transformation::getWindow(x,y,radius,channel,RepeatEdge);
+
+    for(int i=0; i<radius; ++i){
+        for(int q=0; q<radius; ++q){
+
+            tab[zlicz++] = okno(i,q);
+        }
+    }
+
+
+    int mediana;
+
+
+    //sortujemy tablicę
+    int pom;
+    int n = zlicz;
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n-i-1;j++)
+            if(tab[j]>tab[j+1])
+            {
+                pom = tab[j];
+                tab[j] = tab[j+1];
+                tab[j+1] = pom;
+            }
+
+
+    if ( zlicz % 2 == 0 ){ // wartość parzysta
+        mediana = (tab[zlicz/2] + tab[zlicz/2+1]) / 2;
+    } else {
+        mediana = tab[zlicz/2];
+    }
+
+
+
+
+    return mediana;
 }
