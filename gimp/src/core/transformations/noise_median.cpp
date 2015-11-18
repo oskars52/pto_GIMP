83af57bca7dbcb75e2c5801844898690f30e1f70
Oskar Ziegenhagen <oskar1501@gmail.com>
2015-11-18 11:44:47 +0100

    cwiczenia 6 - odszumianie - the last part

--- gimp/src/core/transformations/noise_median.cpp
+++ gimp/src/core/transformations/noise_median.cpp
@@ -24,6 +24,16 @@ PNM* NoiseMedian::transform()
     if (image->format() == QImage::Format_Indexed8)
     {
 
+        for (int x=0; x<width; x++)
+            for (int y=0; y<height; y++)
+            {
+                QRgb pixel = image->pixel(x,y); // Getting the pixel(x,y) value
+
+                int v = getMedian(x,y,LChannel);
+
+                newImage->setPixel(x,y, v);
+            }
+
     }
     else if (image->format() == QImage::Format_RGB32)
     {
